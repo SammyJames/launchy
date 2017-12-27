ae94f155d733371062088e395cbf42602a1d76d6
Sammy James <sammy.james@protonmail.com>
2017-12-27 11:11:04 -0600

    add type_traits to global include and tweak our numeric types

--- src/types.hpp
+++ src/types.hpp
@@ -7,15 +7,16 @@
 #include <memory>
 #include <string>
 #include <tuple>
+#include <type_traits>
 
-using u8  = uint8_t;
-using i8  = int8_t;
-using u16 = uint16_t;
-using i16 = int16_t;
-using u32 = uint32_t;
-using i32 = int32_t;
-using u64 = uint64_t;
-using i64 = int64_t;
+using u8  = unsigned char;
+using i8  = signed char;
+using u16 = unsigned short;
+using i16 = signed short;
+using u32 = unsigned int;
+using i32 = signed int;
+using u64 = unsigned long long;
+using i64 = signed long long;
 using f32 = float;
 using f64 = double;
 
